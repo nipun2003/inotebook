1d073f0aecd5c3a4174ceac4223b802a65faf1ac
Nipun Kumar <nipunvirat0@gmail.com>
2021-12-09 00:34:08 +0530

    Fix Vars

--- helper/emailSender.js
+++ helper/emailSender.js
@@ -1,14 +1,5 @@
 const transporter = require('../config/emailConfig');
 const senderEmail = process.env.EMAIL;
-const hbs = require('nodemailer-express-handlebars');
-transporter.use('compile',hbs({
-   viewEngine:{
-     extname:'.handlebars',
-     defaultLayout:false
-   },
-   viewPath : './view/',
-   defaultLayout:false
-}));
 
 const sendMail = (recieverEmail, otp) => {
     const message = `${otp} is the otp for your iNoteApp. \nDo not reply to this email.`
@@ -17,7 +8,6 @@ const sendMail = (recieverEmail, otp) => {
         to: recieverEmail,   // list of receivers
         subject: "Verification mail",
         text: message,
-        template:'index'
     };
     transporter.sendMail(mailData, function (err, info) {
         if(err)
